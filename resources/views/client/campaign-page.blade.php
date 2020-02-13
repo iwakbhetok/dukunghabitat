a929d9450c74a99ec5bedf560a75456ab1594089
ago_leverate <agofur@leverate.co.id>
2020-02-13 16:13:49 +0700

    added button tentang campaign on personal page

--- resources/views/client/campaign-page.blade.php
+++ resources/views/client/campaign-page.blade.php
@@ -17,6 +17,9 @@
             <p class="uk-text-center">
                 <a class="uk-button uk-modal-close closeBtn uk-text-capitalize" style="font-size: 1rem;color:#fff;background:#0099bc;" href="{{ Voyager::image( $uuid.'/card.gif' ) }}" download>Download GIF</a>
             </p>
+            <p class="uk-text-center">
+                <a class="uk-button uk-modal-close closeBtn uk-text-capitalize" style="font-size: 1rem;color:#0099bc;border: solid 1px #0099bc;width:250px;" href="https://habitatindonesia.org/dukung-habitat/" target="_blank">Tentang Campaign</a>
+            </p>
             <div class="uk-overlay uk-visible@m"></div>
             <div class="uk-overlay uk-hidden@s"></div>
         </div>
